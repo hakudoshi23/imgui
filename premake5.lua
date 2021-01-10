a5abacc54f7b1aceb96f9ee5b31551d80c9e3804
Aleix <aleixvalls14@gmail.com>
2021-01-10 12:15:19 +0100

    update premake5.lua & gitignore

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+        "*.h",
+        "*.cpp",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
